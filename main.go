fd6519573f4f4ac1fd3fb6a6cbb2d7ff42a39160
Inspectre Gadget <inspectre@inspect.re>
2016-06-22 15:30:09 -0400

    Added back-off period, in case of network latency issues. Implemented readlock to prevent race issues with the visited slice. Made the client universal for better performance. Implemented sync.WaitGroup instead of channels for handling syncronization with goroutines.

--- main.go
+++ main.go
@@ -13,21 +13,30 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"time"
 
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 )
 
+// HTTP client that ignores TLS errors
+var client = http.Client{
+	Transport: &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	},
+}
+
 // Visited tracks visited URLs, to avoid redundancy & loops
 type Visited struct {
 	URLS  map[string]bool
-	mutex sync.Mutex
+	mutex sync.RWMutex
 }
 
 var visited Visited
 
-// Whitelist is a group of targets that are allowed to be spidered and
-// searched.
+// Whitelist is a group of targets that are allowed to be spidered and searched.
 // Targets can be either domains or IP addresses, and must contain the scheme (http or https, in this case). Example: http://www.example.com or https://127.0.0.1:8080
 type Whitelist struct {
 	Targets []*url.URL
@@ -35,13 +44,8 @@ type Whitelist struct {
 
 var whitelist Whitelist
 
-// Create the worker pool, used to set the upper limit & track the number of workers in use.
-var workerPool chan struct{}
-var maxWorkers int // Upper limit of workers
-
 // Create the channels used to pass URLs found during spidering
-// TODO: Find the optimal size of buffer.
-var urlQueue = make(chan *url.URL, 100) // arbitrary buffer size, increase for more performance
+var urlQueue chan *url.URL
 
 // The command-line flags
 var flagStartURL = flag.String("url", "", "[REQUIRED] `URL` to start spidering from. The domain and scheme will be used as the whitelist.") // TODO: Allow multiple URLs, comma-separated.
@@ -51,6 +55,9 @@ var flagConcurrency = flag.Uint("concurrency", 3, "Level of concurrency. `0-5`; 
 // provided by the user and calls the router function for any URLs
 // passed into the URL queue.
 func main() {
+	// Change output location of logs (TEMP)
+	log.SetOutput(os.Stdout)
+
 	// Configure the usage message
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
@@ -86,30 +93,26 @@ func main() {
 	switch *flagConcurrency {
 	case 0:
 		// No concurrency
-		maxWorkers = 1
+		urlQueue = make(chan *url.URL, 1)
 	case 1:
 		// Lowest level of concurrency
-		maxWorkers = 5
+		urlQueue = make(chan *url.URL, 5)
 	case 2:
 		// Low level of concurrency
-		maxWorkers = 10
+		urlQueue = make(chan *url.URL, 10)
 	case 4:
 		// High level of concurrency
-		maxWorkers = 50
+		urlQueue = make(chan *url.URL, 50)
 	case 5:
 		// Highest level of concurrency
-		maxWorkers = 100
+		urlQueue = make(chan *url.URL, 100)
 	default:
 		// Medium level of concurrency (-concurrency=3)
-		maxWorkers = 20
+		urlQueue = make(chan *url.URL, 20)
 	}
 
-	// Initialize the worker pool
-	workerPool = make(chan struct{}, maxWorkers)
-	// Fill the worker pool
-	for i := 0; i < maxWorkers; i++ {
-		workerPool <- struct{}{}
-	}
+	// Initialize the wait group
+	var wg sync.WaitGroup
 
 	// Check if the start URL is valid
 	startURLvalue, err := url.Parse(*flagStartURL)
@@ -126,82 +129,76 @@ func main() {
 	whitelist = Whitelist{Targets: []*url.URL{startURLvalue}}
 	urlQueue <- startURLvalue
 
-	// Keep working as long as there are workers working or URLs in the queue
-	for len(workerPool) < maxWorkers || len(urlQueue) > 0 {
-		// If there are URLs available, work with it
-		if len(urlQueue) > 0 {
-			// Take a worker from the pool
-			<-workerPool
-			// Pass a URL to the router, concurrently
-			go dataRouter(<-urlQueue) // TODO: Handle errors
-		}
+	// Add the starting URL to visited to prevent it from being added again
+	addVisited(startURLvalue)
+
+	// Keep working as long as there are URLs in the queue
+	for len(urlQueue) > 0 {
+		go func() {
+			wg.Add(1)
+			dataRouter(<-urlQueue)
+			wg.Done()
+		}()
+		// Add a back-off period, in case of network latency issues
+		time.Sleep(100 * time.Millisecond)
 	}
+
+	// Wait for all processes to finish
+	wg.Wait()
 }
 
 // Function dataRouter requests the given URL, and passes it to various helper functions.
 // It returns any errors it receives throughout this process.
 // Output functionality currently occurs in the helper functions.
 func dataRouter(urlValue *url.URL) (err error) {
-	// Ensure that the worker goes back in the pool before returning
-	defer func() {
-		workerPool <- struct{}{}
-	}()
+	// Set up a wait group for concurrency
+	var wg sync.WaitGroup
 
 	// Remove hashes from the URL
 	urlValue.Fragment = ""
 
-	// Ensure we haven't visited the URL before
-	if _, exists := visited.URLS[urlValue.String()]; exists {
-		// Has already been visited
-		return
-	}
-
-	// Set up an internal worker queue for concurrency
-	internalQueue := make(chan struct{}, 2)
-	for i := 0; i < 2; i++ {
-		internalQueue <- struct{}{}
-	}
-
-	// Configure HTTP client that ignores TLS errors
-	tlsConfig := &tls.Config{
-		InsecureSkipVerify: true,
-	}
-	transport := &http.Transport{
-		TLSClientConfig: tlsConfig,
-	}
-	client := http.Client{
-		Transport: transport,
-	}
-
 	// Get the first URL's document body
 	response, err := client.Get(urlValue.String())
 	if err != nil {
+		log.Printf("[ERROR] [%s] %s\n", urlValue.String(), err.Error())
 		return
 	}
 	defer response.Body.Close() // Make sure the response gets closed
 	document, err := html.Parse(response.Body)
 	if err != nil {
+		log.Printf("[ERROR] [%s] %s\n", urlValue.String(), err.Error())
 		return
 	}
 
 	// Run the spidering function on the html document
-	<-internalQueue
-	go getAnchors(document, urlValue, internalQueue)
+	wg.Add(1)
+	go func() {
+		getAnchors(document, urlValue)
+		wg.Done()
+	}()
 
 	// Search for input fields in the html document
-	<-internalQueue
-	go getInputs(document, urlValue, internalQueue)
-
-	// Don't return until all goroutines have completed
-	<-internalQueue // One for spider function
-	<-internalQueue // One for input finder function
+	wg.Add(1)
+	go func() {
+		getInputs(document, urlValue)
+		wg.Done()
+	}()
 
 	// Add the URL to the visited list, safely
+	addVisited(urlValue)
+
+	// Wait for all the concurrent processes to finish
+	wg.Wait()
+
+	return
+}
+
+// Function addVisited safely adds a given URL value to the visited slice,
+// to prevent it from being tested again.
+func addVisited(urlValue *url.URL) {
 	visited.mutex.Lock()
 	defer visited.mutex.Unlock()
 	visited.URLS[urlValue.String()] = true
-
-	return
 }
 
 // Function getAnchors parses out the links from anchor elements found in the
@@ -209,12 +206,7 @@ func dataRouter(urlValue *url.URL) (err error) {
 // It uses the provided worker pool to perform the task concurrently for the calling function,
 // returning a worker back to the pool upon completion.
 // urlValue is the current URL that it is working with; this is used for contextual logging.
-func getAnchors(document *html.Node, currentURL *url.URL, pool chan struct{}) {
-	// Make sure the workers are returned to the worker pool
-	defer func() {
-		pool <- struct{}{}
-	}()
-
+func getAnchors(document *html.Node, currentURL *url.URL) {
 	// Recursively search the document tree for anchor values
 	var nodeSearch func(*html.Node)
 	nodeSearch = func(node *html.Node) {
@@ -270,6 +262,8 @@ func addURL(urlValue *url.URL) {
 		urlString := urlValue.String()
 
 		// Make sure the URL has not been visited
+		visited.mutex.RLock()
+		defer visited.mutex.RUnlock()
 		if _, exists := visited.URLS[urlString]; !exists {
 			// Add the URL to the queue
 			urlQueue <- urlValue
@@ -300,12 +294,7 @@ func isWhitelisted(urlValue *url.URL) (whitelisted bool) {
 // It uses the worker pool to perform the task concurrently from the calling function,
 // returning the worker to the pool upon completion.
 // urlValue is the current URL that it is working with; this is used for contextual logging.
-func getInputs(document *html.Node, urlValue *url.URL, pool chan struct{}) {
-	// Make sure the workers are returned to the worker pool
-	defer func() {
-		pool <- struct{}{}
-	}()
-
+func getInputs(document *html.Node, urlValue *url.URL) {
 	// Create a slice to hold all the input fields for the current URL
 	var inputs []string
 
@@ -344,3 +333,5 @@ func getInputs(document *html.Node, urlValue *url.URL, pool chan struct{}) {
 		fmt.Println()
 	}
 }
+
+// TODO: Add in the option for verbose logging (all URLs spidered for basic verbosity, and when it reaches spidering/input finding functions in double verbosity)
